3c74d64834d602fe19e443537db4d9fc7bb03461
AdorableWise <tlhalefangmatjila.tm@gmail.com>
2023-02-20 19:30:21 +0200

    Display the forecast data using the API

--- src/app.js
+++ src/app.js
@@ -13,6 +13,53 @@ function formatDate(timestamp){
 let day =days[date.getDay()];
 return `${day} ${hours}: ${minutes}`;
 }
+function formatDay(timestamp){
+  let date= new Date (timestamp*1000);
+  let day = date.getDay();
+  let days =["Sun","Mon","Tues","Wed","Thurs","Fri","Sat"];
+
+  return days[day];
+}
+
+
+function displayForecast(response){
+  let forecast= response.data.daily;
+
+  let forecastElement= document.querySelector("#forecast");
+
+  let forecastHTML=`<div class="row">`;
+  forecast.forEach( function(forecastDay,index){
+    if (index<6){
+      forecastHTML=
+      forecastHTML+
+      `
+      <div class="col-2"
+      <div class="weather-forecast-date">
+      ${formatDay(forecastDay.dt)}</div>
+      
+      <img 
+      src= "http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
+      alt=""
+      width="42"
+      
+      />
+      <div class= "weather-forecast-temperatures">
+      <div class="weather-forecast-temperature-max">${Math.round(forecastDay.temp.max)}°</span>
+      <div class="weather-forecast-temperature-min">${Math.round(forecastDay.temp.min)}°</span>
+      </div>
+      </div>
+      `;
+    }
+    
+    });
+    forecastHTML=forecastHTML+ `</div>`;
+    forecastElement.innerHTML= forecastHTML;
+  }
+
+  
+
+
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
